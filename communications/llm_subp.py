92d68216699b11ad3bd22840241a53cf4ac96899
NikolayBlagoev <workerthreadllc@gmail.com>
2024-12-22 13:18:30 +0200

    deadlock prevention

--- communications/llm_subp.py
+++ communications/llm_subp.py
@@ -222,8 +222,8 @@ class SubP(object):
                     ret = ret.to("cpu")
                     send = isend(ret, task.frm)
                     self.queue_out.put(Backward(task.tag, task.frm, task.to, x.grad.shape[0], x.grad.shape[1], x.grad.shape[2], task.originator, None), True)
-                    if self.iteration == 0:
-                        send.wait()
+                    # if self.iteration == 0:
+                    #     send.wait()
                 elif isinstance(task, Forward):
                     #TODO: Need to receive asynchonously after 1st round
                     if task.data == None:
