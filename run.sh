972bf75e57680585685ed80f34a0a05dcfd5a908
NikolayBlagoev <workerthreadllc@gmail.com>
2024-12-23 11:07:12 +0200

    remove run

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 ps aux | grep python | grep -v "grep python" | awk '{print $2}' | xargs kill -9
 git pull
 rm log*
+rm out*
+rm ERROR_LOG*
 for ((i=$1; i<$2; i=i+1))
 do
     touch "log_stats_proj_2_$i.txt"
