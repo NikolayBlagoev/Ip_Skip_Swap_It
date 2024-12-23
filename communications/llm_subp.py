87b71d3c1ca045f254f437e679d1c4cc94cb901f
NikolayBlagoev <workerthreadllc@gmail.com>
2024-12-23 16:03:05 +0200

    more log

--- communications/llm_subp.py
+++ communications/llm_subp.py
@@ -124,12 +124,14 @@ class SubP(object):
             while self.started:
                 task = None
                 while self.queue_in.empty() and self.started and task == None:
+                    print(len(self.receives))
+
                     for idx,el in enumerate(self.receives):
                         if el[1].is_completed():
                             task = el[0]
                             self.receives.pop(idx)
                             break
-
+                        
                     continue
                 if not self.started:
                     break
